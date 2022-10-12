85e536db9639f6ece33de816fdc27a75c1d0b362
Yanina Maruseva <yanina-ts@mail.ru>
2022-10-12 15:08:06 +0500

    add homework_5

---
+++ hw_Python/hw_05/hw5_4/hw5_4.py
@@ -0,0 +1,39 @@
+# 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+# Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+
+def coding(txt):
+    count = 1
+    res = ''
+    for i in range(len(txt)-1):
+        if txt[i] == txt[i+1]:
+            count += 1
+        else:
+            res = res + str(count) + txt[i]
+            count = 1
+    if count > 1 or (txt[len(txt)-2] != txt[-1]):
+        res = res + str(count) + txt[-1]
+    return res
+
+
+def decoding(txt):
+    number = ''
+    res = ''
+    for i in range(len(txt)):
+        if not txt[i].isalpha():
+            number += txt[i]
+        else:
+            res = res + txt[i] * int(number)
+            number = ''
+    return res
+
+
+with open('hw_Python/hw_05/hw5_4/text.txt', 'r') as file:
+    decoded_string = file.read()
+
+with open('hw_Python/hw_05/hw5_4/text2.txt', 'w') as file:
+    encoded_string = coding(decoded_string)
+    file.write(encoded_string)
+
+print(coding(decoded_string))
+print(decoding(coding(decoded_string)))
