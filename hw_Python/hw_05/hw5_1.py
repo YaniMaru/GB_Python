00ce8e6389bbcd980ad6f362ecf0f662758bfe5c
Yanina Maruseva <yanina-ts@mail.ru>
2022-10-12 15:40:57 +0500

    corrected_hw5_1

--- hw_Python/hw_05/hw5_1.py
+++ hw_Python/hw_05/hw5_1.py
@@ -1,5 +1,4 @@
 # 1. Напишите программу, удаляющую из текста все слова, содержащие "абв".
 
-text = 'Унылая абвг пора, абв, очей очарованье! абвгд?'
-sort_text = list(filter(lambda x: 'абв' not in x, text.split()))
-print(' '.join(sort_text))
+text_str = 'Унылая абвг пора, абв, очей очарованье! абвгд?'
+print(' '.join((filter(lambda x: 'абв' not in x, text_str.split()))))
